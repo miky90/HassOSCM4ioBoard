35c5cab426abb4c4b88b4322bcb456fb44ca50ed
info@doozy.it <Iu78Ewc7tr64>
2022-03-22 00:31:54 +0100

    update log settings

--- cm4iofan/run.sh
+++ cm4iofan/run.sh
@@ -73,8 +73,8 @@ action() {
   cpuTemp=${4}
   CorF=${5}
   fanPercentHex=$(printf '0x%x' "${fanPercent}")
-  #printf '%(%Y-%m-%d_%H:%M:%S)T'
-  #echo ": ${cpuTemp}${CorF} - Level ${fanLevel} - Fan ${fanPercent}% (${fanMode})";
+  test "${logTemp}" == "true" && printf '%(%Y-%m-%d_%H:%M:%S)T'
+  test "${logTemp}" == "true" && echo ": ${cpuTemp}${CorF} - Level ${fanLevel} - Fan ${fanPercent}% (${fanMode})";
   i2cset -y 10 0x2f 0x30 "${fanPercentHex}"
   returnValue=${?}
   test "${createEntity}" == "true" && fanSpeedReport "${fanPercent}" "${fanLevel}" "${fanMode}" "${cpuTemp}" "${CorF}" &
